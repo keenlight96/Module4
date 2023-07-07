90816ef0af910b997c8311054c6e2815c5160bd9
keenlight96 <nguyenkimlong29196@gmail.com>
2023-07-07 14:06:43 +0700

    1

---
+++ Day01/Task01/src/main/java/com/example/task01/controller/GreetingController.java
@@ -0,0 +1,14 @@
+package com.example.task01.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class GreetingController {
+    @GetMapping("/greeting")
+    public String greeting(String name, Model model) {
+        model.addAttribute("name",name);
+        return "index";
+    }
+}
